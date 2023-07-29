1d0d8c95e4772bb0f286fa83275b11ccba1428c8
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-29 10:58:22 +0530

    swag labs

--- selenium_Ztoh/src/selenium_program/pr1.java
+++ selenium_Ztoh/src/selenium_program/pr1.java
@@ -1,14 +1,33 @@
 package selenium_program;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class pr1 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		WebDriver d=new ChromeDriver();
-		d.get("https://www.youtube.com");
+		d.get("https://www.saucedemo.com/");
+		d.manage().window().maximize();
+		Thread.sleep(2000);
+		By uname=By.id("user-name");
+		WebElement username=d.findElement(uname);
+		username.sendKeys("standard_user");
+		Thread.sleep(3000);
+		By pwd=By.id("password");
+		WebElement pwdname=d.findElement(pwd);
+		pwdname.sendKeys("secret_sauce");
+		Thread.sleep(3000);
+		By lgn=By.id("login-button");
+		WebElement login=d.findElement(lgn);
+		login.click();
+		Thread.sleep(5000);
+		d.close();
+		
+		
 
 	}
 
