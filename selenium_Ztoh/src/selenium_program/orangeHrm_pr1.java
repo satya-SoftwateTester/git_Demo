3c5385af1f281b9988603d22675b2ee3ae54e7fa
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-29 11:30:22 +0530

    orangeHrm

---
+++ selenium_Ztoh/src/selenium_program/orangeHrm_pr1.java
@@ -0,0 +1,33 @@
+package selenium_program;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class orangeHrm_pr1 {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		WebDriver d=new ChromeDriver();
+		d.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		d.manage().window().maximize();
+		Thread.sleep(2000);
+		
+		
+		By uname=By.xpath("//input[ @name=\"username\"]");
+		WebElement user=d.findElement(uname);
+		user.sendKeys("Admin");
+		
+		
+		By pname=By.xpath("//input[@name=\"password\"]");
+		WebElement pwdname=d.findElement(pname);
+		pwdname.sendKeys("admin123");
+		By lgn=By.xpath("//button[@class=\"oxd-button oxd-button--medium oxd-button--main orangehrm-login-button\"]");
+		WebElement login=d.findElement(lgn);
+		login.click();
+		
+	
+	}
+
+}
