ec82d64bcc4c58b2eabdf087cd079cf192851009
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-07-31 18:37:43 +0530

    Update orangehrm_crtpfl.java

--- selenium_Ztoh/src/selenium_program/orangehrm_crtpfl.java
+++ selenium_Ztoh/src/selenium_program/orangehrm_crtpfl.java
@@ -1,7 +1,7 @@
 package selenium_program;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
