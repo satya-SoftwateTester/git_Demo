6184715779c85adef0d32eca56c39b7734344a31
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-08-01 11:45:24 +0530

    Update Primus_bank.java

--- selenium_Ztoh/src/selenium_program/Primus_bank.java
+++ selenium_Ztoh/src/selenium_program/Primus_bank.java
@@ -1,6 +1,8 @@
 package selenium_program;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Primus_bank {
@@ -11,6 +13,27 @@ WebDriver d=new ChromeDriver();
 d.get("http://primusbank.qedgetech.com/");
 d.manage().window().maximize();
 Thread.sleep(2000);
+
+By prsnl=By.xpath("//input[@id=\"personal\"]");
+WebElement personal=d.findElement(prsnl);
+personal.click();
+Thread.sleep(2000);
+
+By unm=By.id("idtxt");
+WebElement usrnm=d.findElement(unm);
+usrnm.sendKeys("person");
+Thread.sleep(2000);
+
+By pwd=By.name("pswdtxt");
+WebElement pawd=d.findElement(pwd);
+pawd.sendKeys("person@123");
+//Thread.sleep(5000);
+//pawd.clear();
+Thread.sleep(3000);
+
+By lgn=By.id("btnGO");
+WebElement login=d.findElement(lgn);
+login.click();
 
 
 	}
