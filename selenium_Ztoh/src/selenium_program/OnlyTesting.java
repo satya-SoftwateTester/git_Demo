99b80cc2112a9cc1f3ced1d77ec16109e66f2fee
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-08-02 17:17:33 +0530

    Update OnlyTesting.java

--- selenium_Ztoh/src/selenium_program/OnlyTesting.java
+++ selenium_Ztoh/src/selenium_program/OnlyTesting.java
@@ -1,15 +1,27 @@
 package selenium_program;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlyTesting {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 WebDriver d=new ChromeDriver();
 d.get("http://only-testing-blog.blogspot.com/");
 d.manage().window().maximize();
+Thread.sleep(2000);
+
+By gndprnt=By.id("gparent_1");
+WebElement grandpnt=d.findElement(gndprnt);
+grandpnt.sendKeys("abc10");
+Thread.sleep(2000);
+
+By pnt=By.name("parent1");
+WebElement prnt=d.findElement(pnt);
+prnt.sendKeys("abc20");
 
 
 	}
