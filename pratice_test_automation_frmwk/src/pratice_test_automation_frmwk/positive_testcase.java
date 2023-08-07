cba6c0f1952b2fa020752882fb9adbe0b9e21ee4
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-08-07 18:57:05 +0530

    update

--- pratice_test_automation_frmwk/src/pratice_test_automation_frmwk/positive_testcase.java
+++ pratice_test_automation_frmwk/src/pratice_test_automation_frmwk/positive_testcase.java
@@ -6,15 +6,35 @@ import org.openqa.selenium.WebElement;
 public class positive_testcase extends login_pratice_test_automation
 {
 	
-	By unm=By.xpath("//input[@id=\"username\"]");
+	static By unm=By.xpath("//input[@id=\"username\"]");
 
-	By pwd=By.xpath("//input[@id=\"password\"]");
+	static By pwd=By.xpath("//input[@id=\"password\"]");
 
-	By smt=By.id("submit");
+	static By smt=By.id("submit");
 
-	WebElement usrnm=drv.findElement(unm);
+	static WebElement usrnm=drv.findElement(unm);
 	
-	WebElement pswd=drv.findElement(pwd);
+	static	WebElement pswd=drv.findElement(pwd);
+	
+	static WebElement submit=drv.findElement(smt);
+	
+	public static void unm_action() 
+	{
+		usrnm.sendKeys("student");
+
+	}
+	public static void pwd_action() 
+	{
+		pswd.sendKeys("Password123");
+
+	}
+	public static void smt_action() 
+	{
+		submit.click();
+
+		
+	}
+
 
 
 }
