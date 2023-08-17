1e1c7ae09ea4a7bbe1685e16bf5d45fed5fc95e8
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-08-17 17:44:18 +0530

    new code in w3 school for handling iframe

---
+++ w3school_frmwrk/src/w3school_iframepratice/execution_w3school.java
@@ -0,0 +1,15 @@
+package w3school_iframepratice;
+
+public class execution_w3school {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		driver_w3school.browser();
+		iframe_operation.action_fname();
+		iframe_operation.action_lname();
+		iframe_operation.action_submit();
+		
+		Return_parentpage.action_run();
+	}
+
+}
