a861419c4c5c8b6a4ab68ab31075429f2b30f67c
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-09-01 11:37:17 +0530

    new page

--- Selenium_frmwrk/src/Execution_main/main_class.java
+++ Selenium_frmwrk/src/Execution_main/main_class.java
@@ -1,16 +1,19 @@
 package Execution_main;
 
+import Test.Addcorttest;
 import Test.logintest;
 
 public class main_class {
 
 	public static void main(String[] args) throws InterruptedException
 	{
-		logintest.validcrenditional();
-		logintest.invalid_username();
-		logintest.invalid_password();
-		logintest.empty_unmae_empty_pwd();
-		logintest.invalid_username_invalidpassword();
+//		logintest.validcrenditional();
+//		logintest.invalid_username();
+//		logintest.invalid_password();
+//		logintest.empty_unmae_empty_pwd();
+//		logintest.invalid_username_invalidpassword();
+		Addcorttest.clickaddcort();
+	
 	}
 
 }
---
+++ Selenium_frmwrk/src/Test/Addcorttest.java
@@ -0,0 +1,15 @@
+package Test;
+
+import page.Add_to_Cart;
+
+public class Addcorttest 
+{
+	public static void clickaddcort() throws InterruptedException
+	{
+		logintest.validcrenditional();
+		Add_to_Cart.action_cart();
+		Add_to_Cart.action_ct();
+	
+	}
+
+}
