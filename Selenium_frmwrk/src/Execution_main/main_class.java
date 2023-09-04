9d6c6cf9a1a62ad1eb325e2b47c190c1e60d5e81
satya-SoftwateTester <138883125+satya-SoftwateTester@users.noreply.github.com>
2023-09-04 11:26:59 +0530

    added new test case

--- Selenium_frmwrk/src/Execution_main/main_class.java
+++ Selenium_frmwrk/src/Execution_main/main_class.java
@@ -9,19 +9,21 @@ public class main_class {
 
 	public static void main(String[] args) throws InterruptedException
 	{
-//		logintest.validcrenditional();
-//		logintest.invalid_username();
-//		logintest.invalid_password();
-//		logintest.empty_unmae_empty_pwd();
-//		logintest.invalid_username_invalidpassword();
-	//	Addcorttest.clickaddcort();
-//		Buy_test.empty_first_name();
-//		Buy_test.empty_last_name();
-//		Buy_test.empty_zip_code();
-//		Buy_test.empty();
-//		Buy_test.fill_all();
-		facebooktest.fbvalidcreditional();
-		
+		//		logintest.validcrenditional();
+		//		logintest.invalid_username();
+		//		logintest.invalid_password();
+		//		logintest.empty_unmae_empty_pwd();
+		//		logintest.invalid_username_invalidpassword();
+		//	Addcorttest.clickaddcort();
+		//		Buy_test.empty_first_name();
+		//		Buy_test.empty_last_name();
+		//		Buy_test.empty_zip_code();
+		//		Buy_test.empty();
+		//		Buy_test.fill_all();
+		//facebooktest.fbvalidcreditional();
+		//facebooktest.fbemptyemailname();
+		facebooktest.fbemptypassword();
 	}
+
 
 }
--- Selenium_frmwrk/src/page/Add_to_Cart.java
+++ Selenium_frmwrk/src/page/Add_to_Cart.java
@@ -8,26 +8,26 @@ public class Add_to_Cart extends Login_page
 
 	static By adcrt=By.id("add-to-cart-sauce-labs-backpack");
 	static By crt=By.xpath("//span[@class=\"shopping_cart_badge\"]");
-	
+
 	public static WebElement cart() 
 	{
 		WebElement add=drv.findElement(adcrt);
 		return add;
-		
+
 	}
 	public static WebElement crrt() 
 	{
 		WebElement ct=drv.findElement(crt);
 		return ct;
-		
+
 	}
-	
+
 	public static void action_cart() throws InterruptedException 
 	{
 		cart().click();
 		Thread.sleep(3000);
 	}
-	
+
 	public static void action_ct() 
 	{
 		crrt().click();
